2276870c24e4727cce60dd6fe2792905149492e3
Big-stone <597685607@qq.com>
2017-03-31 17:59:15 +0800

    大功告成

--- js/index.js
+++ js/index.js
@@ -4,7 +4,14 @@
         sectionsColor:['#0da5d6', '#2AB561', '#DE8910', '#16BA9D', '#000'],
         //滚动到某一屏后的回调函数
         afterLoad:function(link,index){
-            console.log(index);
+            console.log(index);/*索引从1开始*/
+            // 排它,把current当做一个开关,哪一屏有current,这一屏就做动画
+            $('.section').removeClass('current');
+            //让动画延迟100ms执行
+            setTimeout(function(){
+                $('.section').eq(index-1).addClass('current');
+            },100)
+
         }
     });
 });
